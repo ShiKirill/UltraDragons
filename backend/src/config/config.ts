5ca89fdb376ed6ef28cbdf5649c1cbcd24aeac60
mvkham <hppynewfear@gmail.com>
2025-07-01 23:12:39 +0800

    fix

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,15 +1,15 @@
 export default () => ({
-    port: parseInt(process.env.PORT || '3000', 10),
-    db: {
-        type: process.env.DB_TYPE || 'postgres',
-        host: process.env.DB_HOST || 'localhost',
-        port: parseInt(process.env.DB_PORT || '5432', 10),
-        username: process.env.DB_USERNAME || 'postgres',
-        password: process.env.DB_PASSWORD || 'postgres',
-        database: process.env.DB_DATABASE || 'cities',
-        ssl: process.env.DB_SSL === 'true',
-        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        synchronize: process.env.NODE_ENV !== 'production',
-        logging: process.env.DB_LOGGING === 'true',
-    }
+  port: parseInt(process.env.PORT || '3001', 10),
+  db: {
+    type: process.env.DB_TYPE || 'postgres',
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT || '5432', 10),
+    username: process.env.DB_USERNAME || 'postgres',
+    password: process.env.DB_PASSWORD || 'postgres',
+    database: process.env.DB_DATABASE || 'cities',
+    ssl: process.env.DB_SSL === 'true',
+    entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+    synchronize: process.env.NODE_ENV !== 'production',
+    logging: process.env.DB_LOGGING === 'true',
+  },
 });
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('port') || 3001;
 
   const config = new DocumentBuilder()
     .setTitle('API')
@@ -14,6 +17,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
